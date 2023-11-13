a1348e0d6188fcd7348205f904794f1c45a52016
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-11-13 11:59:46 +0100

    feat: add price fetching

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,10 +11,18 @@ type OffchainWorkerType = 'local' | 'acurast';
 export class AppComponent {
   title = 'acurast-mina-zkoracle';
 
+  btcPrice: string = '0';
+  ethPrice: string = '0';
+  minaPrice: string = '0';
+
   public selectedOffchainWorkerType: OffchainWorkerType = 'local';
 
   constructor(public readonly oracle: OracleService) {
-    this.oracle.fetchData();
+    this.oracle.getPrices().then((prices) => {
+      this.btcPrice = (Number(prices.btc) / 1000).toString();
+      this.ethPrice = (Number(prices.eth) / 1000).toString();
+      this.minaPrice = (Number(prices.mina) / 1000).toString();
+    });
   }
 
   public setSelectedOffchainWorkerType(type: OffchainWorkerType) {
