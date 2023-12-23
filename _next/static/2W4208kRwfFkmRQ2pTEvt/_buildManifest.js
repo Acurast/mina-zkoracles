fd49edc053389a7d71b4e7b24969693a9b87fc66
Isacco <isacco.sordo@gmail.com>
2023-12-23 10:28:28 +0100

    Deploy gh-pages

---
+++ _next/static/2W4208kRwfFkmRQ2pTEvt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-b866b9268b1342cd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2508d473353f2cb2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
