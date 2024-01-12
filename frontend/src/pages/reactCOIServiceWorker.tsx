2e2aa50f27cc6e3419ae964389b21f417008b92a
IsaccoSordo <isacco.sordo@gmail.com>
2024-01-12 13:47:08 +0100

    fix: broken build

--- frontend/src/pages/reactCOIServiceWorker.tsx
+++ frontend/src/pages/reactCOIServiceWorker.tsx
@@ -4,7 +4,7 @@ export {}
 function loadCOIServiceWorker() {
   if (typeof window !== 'undefined' && window.location.hostname != 'localhost') {
     const coi = window.document.createElement('script');
-    coi.setAttribute('src','/mina-zkoracles/coi-serviceworker.min.js'); // update if your repo name changes for npm run deploy to work successfully
+    coi.setAttribute('src','/coi-serviceworker.min.js'); // update if your repo name changes for npm run deploy to work successfully
     window.document.head.appendChild(coi);
   }
 }
